8eb19c9b725608e2885024796899ad24f9261b83
Go-hiroaki <guangmingwu2010@gmail.com>
2021-01-17 02:36:14 +0900

    Removed files from the index (now ignored)

--- scripts/tes.sh
+++ scripts/tes.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
-
-echo "Evaluate models ...";
-
-#res18ynetsync-VC24-RGB2LAB
-for root in VC24; do
-    for ver in L2LAB; do
-        for net in res18ynetsync; do
-            python src/test.py -checkpoints $net-$root-$ver\_iter_100000.pth;
-        done
-    done
+read -p "Please Specified the Checkpoints ?= " checks;
+for check in $checks; do
+    echo "Evaluating => $check";
+    python src/test.py -checkpoints $check;
 done
 
 echo 'DONE'
